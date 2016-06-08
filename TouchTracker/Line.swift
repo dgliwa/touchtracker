02ac16e00fee7b3dba68f6795c6c3c17649573dc
derekgilwa <dgliwa@thoughtworks.com>
2016-06-08 09:38:19 -0500

    Ability to draw straight lines on the drawview.

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,16 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Derek Gilwa on 6/7/16.
+//  Copyright © 2016 gliwaderek.com. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
